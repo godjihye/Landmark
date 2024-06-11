00e13de256add69c0724acfd02fc6ad0c3f40791
godjihye <76134437+godjihye@users.noreply.github.com>
2024-06-11 17:11:50 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 신지혜 on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
